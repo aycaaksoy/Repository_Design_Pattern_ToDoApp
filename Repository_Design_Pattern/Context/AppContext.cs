9191e7a6b1cbe68357e32dbb79723c64346ece7c
Ayça Aksoy <aycaaksoy2000@gmail.com>
2023-01-09 17:11:18 +0300

    Add project files.

---
+++ Repository_Design_Pattern/Context/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository_Design_Pattern.Models;
+
+namespace Repository_Design_Pattern.Context
+{
+    public class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-60DI5A8;Database=DbToDoApp;User Id=aycaa; Password=admin; integrated security=True;");
+        }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ Repository_Design_Pattern/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Repository_Design_Pattern.Models
+{
+    public class Note
+    {
+        public int NoteID { get; set; }
+        public string NoteName { get; set; }
+        public string NoteTitle { get; set; }   
+        public DateTime NoteDate{ get; set; }   
+
+    }
+}
